59fd5133749f4238bcb2403411909bfd2c11573c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-04-05 19:26:31 +0200

    Fix foreign key

--- services/ratings/src/Infrastructure/Persistence/RatingEntityTypeConfiguration.cs
+++ services/ratings/src/Infrastructure/Persistence/RatingEntityTypeConfiguration.cs
@@ -3,11 +3,6 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using RecommendCoffee.Ratings.Domain.Aggregates.CustomerAggregate;
 using RecommendCoffee.Ratings.Domain.Aggregates.ProductAggregate;
 using RecommendCoffee.Ratings.Domain.Aggregates.RatingAggregate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RecommendCoffee.Ratings.Infrastructure.Persistence;
 
