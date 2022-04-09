808ada31c8f135f8dcb562fc624b2e9762abbc8e
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-04-09 10:09:42 +0200

    Disable mTLS in dapr in favor of istio

--- deploy-dapr.ps1
+++ deploy-dapr.ps1
@@ -5,6 +5,9 @@
     .DESCRIPTION
         This script is used to deploy the dapr operator for the application.
         This should be run before deploying the helm chart.
+
+        Please note that the script disables mTLS as this is handled by the
+        istio service mesh.
 #>
 
 helm upgrade --install dapr dapr/dapr `
@@ -12,4 +15,5 @@ helm upgrade --install dapr dapr/dapr `
     --namespace dapr-system `
     --create-namespace `
     --set global.ha.enabled=false `
+    --set global.mtls.enabled=false `
     --wait
