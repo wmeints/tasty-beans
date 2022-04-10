0d1b57a3e83c81e6079745d989257f03b381ef8c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-04-10 07:31:36 +0200

    Add rowversion

---
+++ services/catalog/sql/catalog-migrations-003.sql
@@ -0,0 +1,85 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220226070147_CreateSchema')
+BEGIN
+    CREATE TABLE [Products] (
+        [Id] uniqueidentifier NOT NULL,
+        [Name] nvarchar(500) NOT NULL,
+        [Description] nvarchar(max) NOT NULL,
+        [Discontinued] bit NOT NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220226070147_CreateSchema')
+BEGIN
+    CREATE TABLE [ProductVariant] (
+        [ProductId] uniqueidentifier NOT NULL,
+        [Id] int NOT NULL IDENTITY,
+        [Weight] int NOT NULL,
+        [UnitPrice] decimal(5,2) NOT NULL,
+        CONSTRAINT [PK_ProductVariant] PRIMARY KEY ([ProductId], [Id]),
+        CONSTRAINT [FK_ProductVariant_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220226070147_CreateSchema')
+BEGIN
+    CREATE INDEX [IX_Products_Name] ON [Products] ([Name]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220226070147_CreateSchema')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220226070147_CreateSchema', N'6.0.3');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220404192251_AddTasteTestingInformation')
+BEGIN
+    ALTER TABLE [Products] ADD [FlavorNotes] nvarchar(max) NULL;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220404192251_AddTasteTestingInformation')
+BEGIN
+    ALTER TABLE [Products] ADD [RoastLevel] int NULL;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220404192251_AddTasteTestingInformation')
+BEGIN
+    ALTER TABLE [Products] ADD [Taste] nvarchar(100) NULL;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220404192251_AddTasteTestingInformation')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220404192251_AddTasteTestingInformation', N'6.0.3');
+END;
+GO
+
+COMMIT;
+GO
+
