3a2ee5012decc9a9491a0c1fbb26e10c550fcb64
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-04-14 15:10:47 +0200

    Fix metric name

--- services/customermanagement/src/Infrastructure/Metrics.cs
+++ services/customermanagement/src/Infrastructure/Metrics.cs
@@ -5,7 +5,7 @@ namespace RecommendCoffee.CustomerManagement.Infrastructure;
 public class Metrics
 {
     private static Meter _meter = new Meter("RecommendCoffee.CustomerManagement.Infrastructure");
-    private static Counter<int> _eventsPublished = _meter.CreateCounter<int>("customermanagement-events-registered");
+    private static Counter<int> _eventsPublished = _meter.CreateCounter<int>("customermanagement-events-published");
 
     public static Counter<int> EventsPublished => _eventsPublished;
 }
