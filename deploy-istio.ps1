e4782163ba2585e1f9bb68571966b81ff9c67179
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-04-18 16:28:36 +0200

    Cleanup helm installation

--- deploy-istio.ps1
+++ deploy-istio.ps1
@@ -19,3 +19,9 @@ istioctl install `
 
 # Next, enable the istio sidecar component auto-injection.
 kubectl label namespace default istio-injection=enabled
+
+# Finally, enable the observability components
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.13/samples/addons/prometheus.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.13/samples/addons/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.13/samples/addons/kiali.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.13/samples/addons/jaeger.yaml
