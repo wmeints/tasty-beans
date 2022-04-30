53f29e3cf3c15b047c2b67033c8bb7d29fed6779
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-04-30 16:05:22 +0200

    Add topic attribute

--- services/shipping/src/domain/Aggregates/ShippingOrderAggregate/Events/ShippingOrderCreatedEvent.cs
+++ services/shipping/src/domain/Aggregates/ShippingOrderAggregate/Events/ShippingOrderCreatedEvent.cs
@@ -1,3 +1,4 @@
 ﻿namespace RecommendCoffee.Shipping.Domain.Aggregates.ShippingOrderAggregate.Events;
 
+[Topic("shipping.shippingorder.created.v1")]
 public record ShippingOrderCreatedEvent(ShippingOrder Order) : IDomainEvent;
