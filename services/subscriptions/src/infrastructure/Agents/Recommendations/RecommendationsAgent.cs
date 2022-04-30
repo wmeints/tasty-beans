ac5cb584506fab9069b9d7763541993f201e5352
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-04-30 14:51:32 +0200

    Fix service agent behavior to better log errors
    Service agents would not log their errors properly so it's quite hard to
    debug problems in the communication between client/server. This change
    fixes that.

--- services/subscriptions/src/infrastructure/Agents/Recommendations/RecommendationsAgent.cs
+++ services/subscriptions/src/infrastructure/Agents/Recommendations/RecommendationsAgent.cs
@@ -1,4 +1,5 @@
 ﻿using Dapr.Client;
+using Microsoft.Extensions.Logging;
 using RecommendCoffee.Subscriptions.Domain.Services.Recommendations;
 
 namespace RecommendCoffee.Subscriptions.Infrastructure.Agents.Recommendations;
@@ -6,19 +7,32 @@ namespace RecommendCoffee.Subscriptions.Infrastructure.Agents.Recommendations;
 public class RecommendationsAgent : IRecommendations
 {
     private readonly DaprClient _daprClient;
+    private readonly ILogger<RecommendationsAgent> _logger;
 
-    public RecommendationsAgent(DaprClient daprClient)
+    public RecommendationsAgent(DaprClient daprClient, ILogger<RecommendationsAgent> logger)
     {
         _daprClient = daprClient;
+        _logger = logger;
     }
 
     public async Task<Guid> GetRecommendProductAsync(Guid customerId)
     {
-        var response =
-            await _daprClient.InvokeMethodAsync<GenerateRecommendationRequest, GenerateRecommendationResponse>(
+        try
+        {
+            var response = await _daprClient.InvokeMethodAsync<GenerateRecommendationRequest, GenerateRecommendationResponse>(
                 HttpMethod.Post, "recommendations", "recommendation",
                 new GenerateRecommendationRequest(customerId));
 
-        return response.ProductId;
+            return response.ProductId;
+        }
+        catch (InvocationException ex)
+        {
+            var responseData = await ex.Response.Content.ReadAsStringAsync();
+
+            _logger.LogError(ex, "Failed to invoke recommendations service. Got response {StatusCode}: {ResponseData}",
+                ex.Response.GetType(), responseData);
+
+            throw ex;
+        }
     }
 }
--- services/subscriptions/src/infrastructure/Agents/Shipping/ShippingAgent.cs
+++ services/subscriptions/src/infrastructure/Agents/Shipping/ShippingAgent.cs
@@ -1,21 +1,40 @@
-﻿using Dapr.Client;
+﻿using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Dapr.Client;
+using Microsoft.Extensions.Logging;
 using RecommendCoffee.Subscriptions.Domain.Services.Shipping;
 using RecommendCoffee.Subscriptions.Domain.Services.Shipping.Commands;
 
 namespace RecommendCoffee.Subscriptions.Infrastructure.Agents.Shipping;
 
-public class ShippingAgent: IShipping
+public class ShippingAgent : IShipping
 {
     private readonly DaprClient _daprClient;
+    private readonly ILogger<ShippingAgent> _logger;
 
-    public ShippingAgent(DaprClient daprClient)
+    public ShippingAgent(DaprClient daprClient, ILogger<ShippingAgent> logger)
     {
         _daprClient = daprClient;
+        _logger = logger;
     }
 
     public async Task CreateShippingOrderAsync(CreateShippingOrderCommand cmd)
     {
         var shippingData = new CreateShippingOrderRequest(cmd.CustomerId, new[] { new OrderItem(cmd.ProductId, 1) });
-        await _daprClient.InvokeMethodAsync(HttpMethod.Post, "shipping", "shippingorders", shippingData);
+
+        try
+        {
+            await _daprClient.InvokeMethodAsync(HttpMethod.Post, "shipping", "shippingorders", shippingData);
+        }
+        catch (InvocationException ex)
+        {
+            var responseData = await ex.Response.Content.ReadAsStringAsync();
+
+            _logger.LogError(ex, "Failed to invoke shipping service. Got response {StatusCode}: {ResponseData}",
+                ex.Response.GetType(), responseData);
+
+            throw ex;
+        }
     }
 }
