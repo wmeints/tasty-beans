e2c16b5b99b126cd093daebeafc5d41e718bacc2
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-05-02 15:49:17 +0200

    Add skeleton for simulation service

---
+++ services/simulation/src/api/Program.cs
@@ -0,0 +1,61 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using TastyBeans.Shared.Diagnostics;
+using TastyBeans.Shared.Infrastructure.EventBus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    })
+    .AddDapr(daprClientBuilder =>
+    {
+        var serializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        serializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddHeaderPropagation();
+builder.Services.AddHealthChecks();
+
+var telemetryOptions = builder.Configuration.GetSection("Telemetry").Get<TelemetryOptions>();
+
+builder.Services.AddTracing(telemetryOptions,
+    "TastyBeans.Catalog.Api",
+    "TastyBeans.Catalog.Application",
+    "TastyBeans.Catalog.Domain",
+    "TastyBeans.Catalog.Infrastructure");
+
+builder.Services.AddMetrics(telemetryOptions,
+    "TastyBeans.Catalog.Api",
+    "TastyBeans.Catalog.Application",
+    "TastyBeans.Catalog.Domain",
+    "TastyBeans.Catalog.Infrastructure");
+
+builder.Services.AddLogging(telemetryOptions);
+
+builder.Services.AddEventPublisher(options => options.DeadLetterTopic = "catalog.deadletter.v1");
+
+var app = builder.Build();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
+
+app.MapHealthChecks("/healthz", new HealthCheckOptions
+{
+    AllowCachingResponses = false
+});
+
+app.UseHeaderPropagation();
+app.UseCloudEvents();
+app.MapSubscribeHandler();
+app.MapControllers();
+
+app.Run();
