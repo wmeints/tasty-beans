1083c4dc7488e5366875c211ab4cc75dfa64c132
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-05-05 18:53:46 +0200

    Add tests for the simulation

---
+++ services/simulation/tests/api/Support/EventObjectFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Bogus;
+
+using TastyBeans.Simulation.Application.IntegrationEvents;
+
+namespace TastyBeans.Simulation.Api.Tests.Support;
+
+public class EventObjectFactory
+{
+    public static CustomerRegisteredEvent Create()
+    {
+        return new CustomerRegisteredEvent(Guid.NewGuid());
+    }
+
+    public static ShipmentDeliveredEvent CreateShipmentDeliveredEvent()
+    {
+        return new ShipmentDeliveredEvent(Guid.NewGuid());
+    }
+
+    public static ShipmentLostEvent CreateShipmentLostEvent()
+    {
+        return new ShipmentLostEvent(Guid.NewGuid());
+    }
+}
---
+++ services/simulation/tests/api/Support/TestServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace TastyBeans.Simulation.Api.Tests.Controllers;
+
+public static class TestServiceExtensions
+{
+    public static Mock<TInterface> ReplaceWithMock<TInterface>(this IServiceCollection services) where TInterface: class
+    {
+        var mock = new Mock<TInterface>();
+        
+        var serviceEntry = services.SingleOrDefault(
+            x => x.ServiceType == typeof(TInterface));
+
+        if (serviceEntry != null)
+        {
+            services.Remove(serviceEntry);
+        }
+
+        services.AddSingleton(mock.Object);
+
+        return mock;
+    }
+}
